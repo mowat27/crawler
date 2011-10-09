8e3c4e7f135c1ce13bebc5a06336ce27b427a0a6
Adrian Mowat <adrian.mowat@gmail.com>
2011-10-09 17:11:35 +0100

    Added support for relative links in crawl

--- lib/crawler/crawler.rb
+++ lib/crawler/crawler.rb
@@ -1,12 +1,13 @@
-require 'open-uri'
+['uri','open-uri'].each {|lib| require lib}
 def crawl(url, max_depth, current_depth = 0, linked_by = {})
+  uri = URI.parse(url)
   begin page = open(url)
-  rescue Exception => ex; return linked_by; end # ignore bad links etc
-  page.read.scan(/href\s*=\s*[\"']([^'"]+)[\"']/)  do  
+  rescue Exception => ex; return linked_by; end # ignore bad links
+  page.read.scan(/href\s*=\s*[\"']([^'"]+)[\"']/)  do 
+    link = $1.start_with?("http") ? $1 : "#{uri.scheme}://#{uri.host}:#{uri.port}#{$1}"
     case 
-      when !$1.start_with?("http"); next
-      when !linked_by[$1].nil?; linked_by[$1] << url
-      else linked_by[$1] = [url]; crawl($1, max_depth, current_depth + 1, linked_by) unless current_depth >= max_depth
+      when !linked_by[link].nil?; linked_by[link] << url
+      else linked_by[link] = [url]; crawl(link, max_depth, current_depth + 1, linked_by) unless current_depth >= max_depth
     end
   end
   linked_by
