493ffb6cf783ae62a2344df5bf98dc6bf3842812
Adrian Mowat <adrian.mowat@gmail.com>
2011-10-09 14:30:17 +0100

    Created a simple test mechanism and condensed the implementation of crawl

--- crawler.rb
+++ crawler.rb
@@ -1,19 +1,24 @@
-['open-uri','set'].each {|lib| require lib}
+['open-uri'].each {|lib| require lib}
 def crawl(url,  max_depth = 10, current_depth = 0, linked_by = {})
   begin page = open(url)
   rescue Exception => ex; return linked_by; end # ignore bad links etc
-  links_found = page.inject(Set.new) do |found, line| 
-    line.scan(/href *= *[\"']([^'"]+)[\"']/) {|m| found << $1 if $1.start_with?("http")} 
-    found
-  end
-  links_found.each do |link|
-    if(linked_by[link]) 
-      linked_by[link] << url
-    else
-      linked_by[link] = [url]
-      linked_by = crawl(link, max_depth, current_depth + 1, linked_by) unless current_depth >= max_depth
+  page.string.scan(/href *= *[\"']([^'"]+)[\"']/)  do  
+    case 
+      when !$1.start_with?("http"); next
+      when !linked_by[$1].nil?; linked_by[$1] << url
+      else linked_by[$1] = [url]; crawl($1, max_depth, current_depth + 1, linked_by) unless current_depth >= max_depth
     end
   end
   linked_by
 end
-puts "#{crawl(ARGV[0] || 'http://localhost:4567', ARGV[1].to_i || 2)}"
+new_version = crawl(ARGV[0] || 'http://localhost:4567', ARGV[1].to_i || 2)
+
+expected = {"http://localhost:4567/page1"=>["http://localhost:4567", "http://localhost:4567/page1", "http://localhost:4567/page2", "http://localhost:4567"], "http://localhost:4567/page2"=>["http://localhost:4567/page1", "http://localhost:4567"], "http://localhost:4567/page3"=>["http://localhost:4567/page2"], "http://localhost:4567/page99"=>["http://localhost:4567/page2"]}
+
+if(expected == new_version)
+  puts "Test Passed"
+else
+  puts "Test failed"
+  puts "expected: #{expected}"
+  puts "new_version: #{new_version}"
+end
